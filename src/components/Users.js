0338ef29c6db23b6ecea76e478d7ce538ac250b1
gege0121 <wanggege0121@gmail.com>
2021-01-11 11:36:52 -0500

    add change state button

--- src/components/Users.js
+++ src/components/Users.js
@@ -7,11 +7,20 @@ class Users extends React.Component{
         name: 'gege',
         age: 25
     }
+
+    changeMyAge=()=>{
+        this.setState({age:21 });
+
+    }
+
+
     render(){
         return(
             <div>
             <h1> {this.state.name}</h1>
             <h2>{this.state.age}</h2>
+            <button onClick={this.changeMyAge}>Change My Age</button>
+            <hr/>
             </div>
         );
     }
