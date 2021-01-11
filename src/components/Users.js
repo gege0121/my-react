f3c2516f558eba72c3b2f7da20b48cb6f24c1e1b
gege0121 <wanggege0121@gmail.com>
2021-01-11 12:23:08 -0500

    example: Change in state re-render the UI

--- src/components/Users.js
+++ src/components/Users.js
@@ -8,6 +8,17 @@ class Users extends React.Component{
         age: 25
     }
 
+    data={
+        city: 'Falls Church'
+    }
+
+    changeMyCity=()=>{
+        console.log('change my city from here');
+        console.log('before change', this.data.city);
+        this.data.city='FairFax';
+        console.log('after change',this.data.city);
+    }
+
     changeMyAge=()=>{
         this.setState({age:21 });
 
@@ -15,12 +26,15 @@ class Users extends React.Component{
 
 
     render(){
+        console.log('Im a render()');
         return(
             <div>
             <h1> {this.state.name}</h1>
             <h2>{this.state.age}</h2>
             <button onClick={this.changeMyAge}>Change My Age</button>
             <hr/>
+            <h3>{this.data.city}</h3>
+            <button onClick={this.changeMyCity}>Change My City</button>
             </div>
         );
     }
