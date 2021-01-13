7d8bf59489bee0caf39f54613066b914205c362a
gege0121 <wanggege0121@gmail.com>
2021-01-12 22:49:27 -0500

    addbook in ui

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -3,19 +3,35 @@ import Book from './Book';
 
 export default class Books extends React.Component{
 state={
-    rating: ''
+    name:'',
+    rating: '',
+    books:[]
 }
 
-handleRating =(rating)=>{
-    this.setState({rating:rating});
+handleRatingChange =(event)=>{
+    this.setState({rating:event.target.value});
 
+}
+
+handleNameChange =(event)=>{
+    this.setState({name: event.target.value});
+
+}
+handleAddChange =()=>{
+    const updatedBooks=this.state.books;
+    updatedBooks.push({name:this.state.name,rating:this.state.rating});
+    console.log('updatedBooks',updatedBooks);
 }
 
     render(){
         return(
-            <div>
-                <Book name='The kite Runner' showRating={this.handleRating}/>
-                <h3>Rating:{this.state.rating}</h3>
+            <div className='books'> 
+            <div className='add-books'>
+                <input type="text" placeholder='Enter book name' onChange={this.handleNameChange}/>
+                <input type="text" placeholder='Enter book rate' onChange={this.handleRatingChange}/>
+                <button onClick={this.handleAddChange}>Add</button>
+
+                </div>
             </div>
         )
     }
