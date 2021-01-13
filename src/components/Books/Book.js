f43191d73bb7593d9355a6fd2444ad3a9615763a
gege0121 <wanggege0121@gmail.com>
2021-01-13 00:00:44 -0500

    clik the book to delete use filter

--- src/components/Books/Book.js
+++ src/components/Books/Book.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Book(props){
     return(
-        <div className='book'>
+        <div className='book' onClick={()=>{props.deleteBook(props.book)}}>
             <h1>{props.book.name}</h1>
            <h3>{props.book.rating}</h3>
         </div>
--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Book from './Book';
 
 export default class Books extends React.Component{
+
 state={
     name:'',
     rating: '',
     books:[]
 }
+
+
 
 handleRatingChange =(event)=>{
     this.setState({rating:event.target.value});
@@ -23,10 +26,17 @@ handleAddChange =()=>{
     this.setState({books:updatedBooks, name:'', rating:''});
 }
 
+handleDelete=(deletebook)=>{
+    console.log('start to delete',deletebook);
+    const books=this.state.books;
+    this.setState({books:books.filter(book=>book.name!==deletebook.name)})
+}
+
     render(){
         return(
             <div className='books'> 
             <div className='add-books'>
+                
                 <input type="text" placeholder='Enter book name' onChange={this.handleNameChange} value={this.state.name}/>
                 <input type="text" placeholder='Enter book rate' onChange={this.handleRatingChange} value={this.state.rating}/>
                 <button onClick={this.handleAddChange}>Add</button>
@@ -34,7 +44,7 @@ handleAddChange =()=>{
                 </div>
                 <div>
                     {
-                        this.state.books.map(book =><Book book={book}/>)
+                        this.state.books.map(book =><Book book={book} deleteBook={this.handleDelete} key={this.state.name}/>)
                     }
                 </div>
             </div>
